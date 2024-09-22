00634cb3446bec5cf30a420b5a33469fa42eb2af
PhuongNgo0311 <128676713+PhuongNgo0311@users.noreply.github.com>
2024-09-22 10:59:21 +0700

    Add files via upload

---
+++ week4/App.js
@@ -0,0 +1,18 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+import Screen2c from './components/Screen2c'
+import Tiki_Ok from './components/Tiki_OK'
+import Count from './components/Count'
+import Count2 from './components/Count2'
+export default function App() {
+ // return <Screen2c />;
+
+// return <Tiki_Ok />;
+
+//return <Count />;
+//return < Count2/>;
+
+}
